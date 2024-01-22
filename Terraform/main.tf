5deceab943b46fac03eba042cc8ade1a756a9367
DairymilkJagan <jagansenthil1742001@gmail.com>
2024-01-22 16:39:48 +0530

    Terraform--Update

---
+++ Terraform/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  credentials = file("D:\\Product Task\\task\\Terraform\\terraform-411609-2016aed69555.json")
+  project     = "terraform-411609"
+  region      = "us-central1"
+}
+
+# 1 Create any one resource using terraform in any cloud.
+resource "google_storage_bucket" "my_bucket" {
+  name     = var.bucket_name
+  location = var.bucket_location
+}
+# 2 Try local and cloud storage backend configuration to store state files.
+# 3 Try to use depends on module in code.
+data "terraform_remote_state" "store" {
+  depends_on = [google_storage_bucket.my_bucket]
+  backend = "gcs"
+  config = {
+    bucket  = "demo-task"
+    prefix  = "statefile"
+  }
+}
+#5. Print the output of a resource which you have created.
+output "bucket_url" {
+  value = google_storage_bucket.my_bucket.url
+}
---
+++ Terraform/variable.tf
@@ -0,0 +1,10 @@
+# 4 Try static and dynamic values passing for the variables that will be used by the resource during creationtime.
+variable "bucket_name" {
+  type    = string
+  default = "demo-task"
+}
+# 4
+variable "bucket_location" {
+  type    = string
+
+}
